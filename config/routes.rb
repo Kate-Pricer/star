a3855e04cd4624337a13c59eaad05d42be36bdd5
Chris Hallendy <chris.hallendy@gmail.com>
2014-01-07 12:33:44 -0500

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Starbucks::Application.routes.draw do
-  get '/index' => "pages/index"
-  get '/home' => "pages/home"
- 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-  # ...
-  # You can have the root of your site routed with "root"
-  root :to => "pages#index"
+  get '/index' => "page#index"
+  get '/home' => "page#home"
+  root :to => "page#index"
 end
